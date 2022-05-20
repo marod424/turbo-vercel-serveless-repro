f76c78d453325c6ba8ce51cc3846a58ba9ee0b63
matthew <marod424@gmail.com>
2022-05-20 15:08:49 -0400

    add swr

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 import { MantineProvider, theme } from 'ui';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider theme={theme}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <SWRConfig value={{ refreshInterval: 2000 }}>
+      <MantineProvider theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SWRConfig>
   )
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,13 @@
+import useSWR from "swr";
 import { Button } from "ui";
 
+const fetcher = (url: any) => fetch(url).then((r) => r.json())
+
 export default function Web() {
+  const { data } = useSWR('/api/user', fetcher)
+
+  console.log('data', data)
+
   return (
     <div>
       <h1>Web</h1>
